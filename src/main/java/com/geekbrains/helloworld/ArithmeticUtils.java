bfb1a99646ad5b6a4118de3c6400d21250b7ae10
Михаил Левин <mikelevin@yandex-team.ru>
2021-09-06 12:34:37 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/helloworld/ArithmeticUtils.java
@@ -0,0 +1,10 @@
+package com.geekbrains.helloworld;
+
+public final class ArithmeticUtils {
+
+    private ArithmeticUtils() {}
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/geekbrains/helloworld/ArithmeticUtilsTest.java
@@ -0,0 +1,30 @@
+package com.geekbrains.helloworld;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class ArithmeticUtilsTest {
+
+    private static Stream<Arguments> params() {
+        return Stream.of(
+                arguments(10, 7, 17),
+                arguments(15, 10, 25),
+                arguments(1, 2, 3)
+        );
+    }
+
+    @DisplayName("Сумма двух чисел")
+    @ParameterizedTest(name = "Тест № {index}.  {0} + {1} = {2}")
+    @MethodSource("params")
+    void sum(int a, int b, int expected) {
+        assertEquals(expected, ArithmeticUtils.sum(a, b));
+    }
+
+}
